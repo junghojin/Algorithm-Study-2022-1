97356be9c007b9b4b1f25e5123ee311376cd36bc
david <abovenormal5023@gmail.com>
2022-05-07 17:19:21 +0900

    진우의 달 여행 boj 17485

--- 한세환/dp/Main_17485.java
+++ 한세환/dp/Main_17485.java
@@ -31,12 +31,14 @@ public class Main_17485 {
         int dp[][][] = new int[n][m][3];
         int answer = Integer.MAX_VALUE;
 
+        //===초기화====
         for (int i = 0; i < m; i++) {
             dp[0][i][0] = arr[0][i];
             dp[0][i][1] = arr[0][i];
             dp[0][i][2] = arr[0][i];
         }
 
+        //==가능하지 않는 범위에서 왔을때 최대값으로 초기화==
         for (int i = 0; i < n; i++) {
             dp[i][0][0] = Integer.MAX_VALUE;
             dp[i][m - 1][2] = Integer.MAX_VALUE;
@@ -44,12 +46,12 @@ public class Main_17485 {
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (j == 0) {
+                if (j == 0) {   // 맨 왼쪽에 있을때
                     dp[i][j][1] += dp[i - 1][j + 1][0] + arr[i][j]; // ↙에서 ↓
                     dp[i][j][2] += dp[i - 1][j + 1][0] + arr[i][j]; // ↙에서 ↘
 
                     dp[i][j][2] = dp[i][j][2] == 0 ? dp[i - 1][j][1] + arr[i][j] : Math.min(dp[i][j][2], dp[i - 1][j][1] + arr[i][j]); // ↓에서 ↘
-                } else if (j == m - 1) {
+                } else if (j == m - 1) {    // 맨 오른쪽에 있을 때
                     dp[i][j][1] += dp[i - 1][j - 1][2] + arr[i][j]; // ↘에서 ↓
                     dp[i][j][0] += dp[i - 1][j][1] + arr[i][j]; // ↓에서 ↙
 
@@ -67,9 +69,6 @@ public class Main_17485 {
         }
         for (int j = 0; j < m; j++) {
             for (int k = 0; k < 3; k++) {
-                if (dp[n - 1][j][k] == 0) {
-                    continue;
-                }
                 answer = Math.min(answer, dp[n - 1][j][k]);
             }
         }
